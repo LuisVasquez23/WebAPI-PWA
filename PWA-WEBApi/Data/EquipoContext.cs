a333dfee754a9dda4492261fc57f5434a8948291
Luis Vásquez <vluis5457@gmail.com>
2023-02-25 17:24:38 -0600

    Agregar archivos de proyecto.

---
+++ PWA-WEBApi/Data/EquipoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PWA_WEBApi.Models;
+
+namespace PWA_WEBApi.Data
+{
+    public class EquipoContext : DbContext
+    {
+
+        public EquipoContext(DbContextOptions<EquipoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Equipo> Equipos { get; set; }
+    }
+}
